24dc836f8216ce5e4f7a3e31cec304bc4d7ebf9a
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-30 13:13:06 +0500

    loops

---
+++ reversed_string.py
@@ -0,0 +1,6 @@
+string=input("enter any word :")
+reversed_string=""
+for i in string:
+    reversed_string = i+reversed_string
+print("the orignal word is " , string)
+print("the reversed word is " , reversed_string)
