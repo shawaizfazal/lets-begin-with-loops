5fb95c2b877020df967d791da2ec5269a2db381d
shawaizfazal <shazmaxfazal@gmail.com>
2026-07-01 11:59:21 +0500

    check age

---
+++ check_age.py
@@ -0,0 +1,11 @@
+#take input from the user if he or she is between 10 or 20 years old
+age=input("is your age between 10 to 15 years old?(Y/N) :").strip().upper()
+#check the users input and take outcome exactly or accordingly
+if age=="Y": #condition1
+    print("you are allowed")
+else:
+    age=input("is your age between 15 to 20")
+    if age:str<=20
+    print("you are allowed")
+else:
+print("you are not allowed")
